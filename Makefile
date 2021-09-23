80270b1621056331f92b2146cdc6592b73a4a346
Fabian Thomas <fabian@fabianthomas.de>
2021-09-23 15:16:41 +0200

    add rofi script

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ endif
 
 install:
 	mkdir -p $(DESTDIR)$(PREFIX)/bin
-	cp -f bin/bookmark bin/dmenu-bookmark $(DESTDIR)$(PREFIX)/bin/
-	chmod 755 $(DESTDIR)$(PREFIX)/bin/bookmark $(DESTDIR)$(PREFIX)/bin/dmenu-bookmark
+	cp -f bin/bookmark bin/dmenu-bookmark bin/rofi-bookmark bin/rofi-bookmark-download.py $(DESTDIR)$(PREFIX)/bin/
+	chmod 755 $(DESTDIR)$(PREFIX)/bin/bookmark $(DESTDIR)$(PREFIX)/bin/dmenu-bookmark $(DESTDIR)$(PREFIX)/bin/rofi-bookmark $(DESTDIR)$(PREFIX)/bin/rofi-bookmark-download.py
 
 uninstall:
-	rm -f $(DESTDIR)$(PREFIX)/bin/bookmark $(DESTDIR)$(PREFIX)/bin/dmenu-bookmark
+	rm -f $(DESTDIR)$(PREFIX)/bin/bookmark $(DESTDIR)$(PREFIX)/bin/dmenu-bookmark $(DESTDIR)$(PREFIX)/bin/rofi-bookmark $(DESTDIR)$(PREFIX)/bin/rofi-bookmark-download.py
---
+++ bin/rofi-bookmark-download.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import os
+import subprocess
+import tldextract
+import requests
+import sys
+
+retry = len(sys.argv) > 1 and sys.argv[1] == '--retry'
+
+bookmarks_dir = os.environ['BOOKMARKS_DIR'] if 'BOOKMARKS_DIR' in os.environ else os.path.join(os.environ['HOME'], '.bookmarks')
+cache_dir = os.environ['CACHE_DIR']
+cache_rofi = os.environ['CACHE_ROFI']
+
+os.makedirs(cache_dir, exist_ok=True)
+
+bookmarks = {}
+url_icons = {}
+with subprocess.Popen(['bookmark', 'list'], stdout=subprocess.PIPE) as proc:
+    stdout, _ = proc.communicate()
+    for line in stdout.splitlines():
+            name = line.decode()
+            with open(os.path.join(bookmarks_dir, name), 'r') as file:
+                url = file.readline()
+                # get second level domain
+                res = tldextract.extract(url)
+                second_level = '.'.join(res[1:])
+                bookmarks[name] = second_level
+                if second_level not in url_icons:
+                    if second_level.endswith('github.com'):
+                        url_icons[second_level] = (second_level + '.svg', 'https://github.githubassets.com/favicons/favicon-dark.svg')
+                    else:
+                        ico = second_level + '.ico'
+                        url_icons[second_level] = (ico, 'https://icons.duckduckgo.com/ip3/' + ico)
+
+# download icons
+for url in url_icons:
+    filepath = os.path.join(cache_dir, url_icons[url][0])
+    if not os.path.exists(filepath) or (retry and os.stat(filepath).st_size == 0):
+        url = url_icons[url][1]
+        res = requests.get(url)
+        if res.status_code == requests.codes.ok:
+            with open(filepath, 'wb') as file:
+                file.write(res.content)
+        else:
+            open(filepath, 'w').close()
+
+# prepare rofi input
+rofi = ''
+for name in bookmarks:
+    second_level = bookmarks[name]
+    ico_filepath = os.path.join(cache_dir, url_icons[second_level][0])
+    if os.path.exists(ico_filepath) and os.stat(ico_filepath).st_size != 0:
+        rofi += name + '\0icon\x1f' + ico_filepath + '\n'
+    else:
+        rofi += name + '\n'
+
+with open(cache_rofi, 'w') as file:
+    file.write(rofi)
